3c22f3fd0df3e4962d3df3b2c101ba0116f90959
Nishantviroja <nishuviroja@gmail.com>
2025-11-14 17:37:05 +0530

    add test ad.s

--- app/data/integrations.js
+++ app/data/integrations.js
@@ -6,7 +6,7 @@ const integrations = {
   // AdSense Test Mode Control
   // Set to true: See test ads on your domain (no revenue, safe testing)
   // Set to false: See real ads (earn revenue)
-  ADSENSE_TEST_MODE: true,
+  ADSENSE_TEST_MODE: false,
 };
 
 export default integrations;
